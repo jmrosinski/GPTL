92566ab495d705fe4c7bc4ae802df25a6a8f10b7
rosinski <rosinski>
2004-12-25 22:53:32 +0000

    Add avail.c test: lists available PAPI options on this arch.

---
+++ tests/avail.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../private.h"
+
+#ifdef HAVE_PAPI
+#include <papi.h>
+#endif
+
+int main ()
+{
+#ifdef HAVE_PAPI
+
+  int i;
+  int ret;
+  PAPI_event_info_t info;
+
+  if ((ret = PAPI_library_init (PAPI_VER_CURRENT)) != PAPI_VER_CURRENT) {
+    printf (PAPI_strerror (ret));
+    return -1;
+  }
+  
+  printf("Name                Code        Description");
+  i = PAPI_PRESET_MASK;
+
+  do {
+    ret = PAPI_get_event_info (i, &info);
+    if (ret == PAPI_OK) {
+      printf("%-20s %-10d %s\n", info.symbol, i & info.event_code, info.long_descr);
+    }
+  } while (PAPI_enum_event (&i, PAPI_ENUM_ALL) == PAPI_OK);
+#else
+  printf ("PAPI not enabled so this code does nothing\n");
+#endif
+}
