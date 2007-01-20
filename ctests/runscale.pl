9ba77168f36b9069a0f96cf17eba3419aa3869d4
rosinski <rosinski>
2007-01-20 15:45:55 +0000

    *** empty log message ***

--- ctests/runscale.pl
+++ ctests/runscale.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 
 use Sys::Hostname;
+use Cwd;
 
 my ($host) = hostname;
 my ($cmdfile) = "scale.gp";
@@ -12,6 +13,9 @@ my (@fpopsfiles) = ("FPops_aggregate","FPops_max","FPops_min");
 my (@membwfiles) = ("MemBW_aggregate","MemBW_max","MemBW_min");
 my (@mpisendrecvfiles) = ("MPI_Sendrecv_aggregate","MPI_Sendrecv_max","MPI_Sendrecv_min");
 my ($mpicmd) = "mpirun";
+my ($rundir) = ".";
+my ($arg);
+my ($cwd);
 
 # Remove old files
 
@@ -19,7 +23,27 @@ unlink (@fpopsfiles);
 unlink (@membwfiles);
 unlink (@mpisendrecvfiles);
 
+# Parse arg list
+
+chomp (@ARGV);
+while (defined ($arg = shift (@ARGV))) {
+    if ($arg eq "-help") {
+	print (STDOUT "Usage: $0 [-help] [-mpicmd cmd] [-rundir dir]\n");
+	exit 0;
+    } elsif ($arg eq "-mpicmd") {
+	defined ($mpicmd = shift (@ARGV)) || die ("Bad arg to -mpicmd\n");
+    } elsif ($arg eq "-rundir") {
+	defined ($rundir = shift (@ARGV)) || die ("Bad arg to -rundir\n");
+	chdir $rundir                     || die ("Unable to chdir to $rundir\n");
+    } else {
+        die ("Unknown arg encountered: $arg\n");
+    }
+}
+
 # Run the test cases
+
+$cwd = getcwd;
+print (STDOUT "Tests will be run in directory $cwd\n");
 
 foreach $nodecount (@nodecounts) {
     $cmd = "$mpicmd -np $nodecount ./scale ";
@@ -49,17 +73,17 @@ print (CMDFILE "set xlabel \"Number of MPI tasks\"\n");
 &getlinear ($fpopsfiles[0], "fpopslinear");
 print (CMDFILE "set ylabel \"MFlops/sec\"\n");
 print (CMDFILE "set output 'FPops.ps'\n");
-print (CMDFILE "plot '$fpopsfiles[0]' using 1:2, '$fpopsfiles[1]' using 1:2, '$fpopsfiles[2]' using 1:2, 'fpopslinear' using 1:2\n");
+print (CMDFILE "plot '$fpopsfiles[0]' using 1:2, '$fpopsfiles[1]' using 1:2, '$fpopsfiles[2]' using 1:2, 'fpopslinear' using 1:2 with lines\n");
 
 &getlinear ($membwfiles[0], "membwlinear");
 print (CMDFILE "set ylabel \"MB/sec\"\n");
 print (CMDFILE "set output 'MemBW.ps'\n");
-print (CMDFILE "plot '$membwfiles[0]' using 1:2, '$membwfiles[1]' using 1:2, '$membwfiles[2]' using 1:2, 'membwlinear' using 1:2\n");
+print (CMDFILE "plot '$membwfiles[0]' using 1:2, '$membwfiles[1]' using 1:2, '$membwfiles[2]' using 1:2, 'membwlinear' using 1:2 with lines\n");
 
 &getlinear ($mpisendrecvfiles[0], "mpisendrecvlinear");
 print (CMDFILE "set ylabel \"MB/sec\"\n");
 print (CMDFILE "set output 'MPI_Sendrecv.ps'\n");
-print (CMDFILE "plot '$mpisendrecvfiles[0]' using 1:2, '$mpisendrecvfiles[1]' using 1:2, '$mpisendrecvfiles[2]' using 1:2, 'mpisendrecvlinear' using 1:2\n");
+print (CMDFILE "plot '$mpisendrecvfiles[0]' using 1:2, '$mpisendrecvfiles[1]' using 1:2, '$mpisendrecvfiles[2]' using 1:2, 'mpisendrecvlinear' using 1:2 with lines\n");
 
 close (CMDFILE);
 
