a87e19a9a8b31c5f131c3561b6a2c6b0fd790168
rosinski <rosinski>
2008-04-05 14:16:28 +0000

    Update doc. Get rid of cfort.h

--- cfort.h
+++
@@ -1,5 +0,0 @@
-#if ( defined GFORTRAN ) || ( defined G95 ) || ( defined LINUX )
-#define FORTRANDOUBLEUNDERSCORE
-#elif ( defined SUNOS ) || ( defined IRIX64 ) || ( defined OSF1 ) || ( defined NEC_SX ) || ( defined UNICOSMP )
-#define FORTRANUNDERSCORE
-#endif
