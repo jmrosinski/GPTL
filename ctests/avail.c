deaccd2fda98039f7fb970fb48e0b1d4f0ace73d
rosinski <rosinski>
2008-08-20 01:50:43 +0000

    Fix tests: don't include papi.h in Fortran

--- ctests/avail.c
+++ ctests/avail.c
@@ -22,8 +22,9 @@ int main ()
   printf ("Purpose: print derived events available on this architecture\n");
   printf ("For PAPI-specific events, run papi_avail and papi_native_avail"
 	  " from the PAPI distribution\n\n");
-  printf ("Derived events:\n");
+  printf ("Available derived events:\n");
   printf ("Name                Code        Description\n");
+  printf ("-------------------------------------------\n");
 
   /*
   ** This should be done with a "getter" function for the derived counter name
@@ -31,11 +32,11 @@ int main ()
 
   if ((ret == PAPI_query_event (PAPI_TOT_INS)) == PAPI_OK &&
       (ret == PAPI_query_event (PAPI_TOT_CYC)) == PAPI_OK)
-    printf("%-20s %-10d %s\n", "GPTL_IPC", GPTL_IPC, "Instructions per cycle\n");
+    printf("%-20s %-10d %s\n", "GPTL_IPC", GPTL_IPC, "Instructions per cycle");
 
   if ((ret == PAPI_query_event (PAPI_FP_OPS)) == PAPI_OK &&
       (ret == PAPI_query_event (PAPI_LST_INS)) == PAPI_OK)
-    printf("%-20s %-10d %s\n", "GPTL_IPC", GPTL_CI, "Computational intensity\n");
+    printf("%-20s %-10d %s\n", "GPTL_CI", GPTL_CI, "Computational intensity");
   
 #else
   printf ("PAPI not enabled so this code does nothing\n");
