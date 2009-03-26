0f53eb4d399bca38888ceff4acff2a000c7efbf2
rosinski <rosinski>
2009-03-26 02:40:11 +0000

    Add profcxx to ctests/

---
+++ ctests/myclasses.h
@@ -0,0 +1,52 @@
+class Base
+{
+ public:
+  Base ()
+  {
+  }
+  ~Base ()
+  {
+  }
+};
+
+class X: Base
+{
+ public:
+  X () 
+  {
+  }
+  ~X() 
+  {
+  }
+  void func (int x)
+  {
+  }
+  void func (double x)
+  {
+  }
+};
+
+class Y: Base
+{
+ public:
+  Y ();
+  ~Y();
+  void func (int);
+  void func (double);
+};
+
+Y::Y ()
+{
+}
+
+Y::~Y()
+{
+}
+
+void Y::func (int x)
+{
+}
+
+void Y::func (double x)
+{
+}
--- ctests/profcxx.C
+++ ctests/profcxx.C
@@ -1,13 +1,26 @@
 #include "../gptl.h"
+#include "myclasses.h"
 
 int main ()
 {
   X *x;
   Y *y;
+  int ret;
+
+  ret = GPTLinitialize ();
+  ret = GPTLstart ("total");
 
   x = new (X);
-  x.func (1.2);
-  x.func (1);
-
+  x->func (1.2);
+  x->func (1);
   delete (x);
+
+  y = new (Y);
+  y->func (1.2);
+  y->func (1);
+  delete (y);
+
+  ret = GPTLstop ("total");
+  ret = GPTLpr (0);
 }
+
