c16a18ce39c9baa13a7e55cdc8cb3c64b76a0408
Jim Rosinski <jmrosinski@gmail.com>
2017-09-15 10:05:30 -0600

    o Add -Mcuda=cuda8 to CFLAGS for gptl.c build o Add MPI_Barrier calls in persist_mpi.F90 to avoid intermixing printout

--- acctests/persist_mpi.F90
+++ acctests/persist_mpi.F90
@@ -22,21 +22,29 @@ program persist
 
   call mpi_init (ierr)
   write(6,*)'calling mpi_comm_rank'
+  call mpi_barrier (MPI_COMM_WORLD, ierr)
   call mpi_comm_rank (MPI_COMM_WORLD, myrank, ierr)
   write(6,*)'myrank=',myrank
+  call mpi_barrier (MPI_COMM_WORLD, ierr)
   write(6,*)'calling acc_init'
+  call mpi_barrier (MPI_COMM_WORLD, ierr)
   call acc_init (ACC_DEVICE_NVIDIA)
   write(6,*)'calling acc_get_num_devices'
+  call mpi_barrier (MPI_COMM_WORLD, ierr)
   ngpus = acc_get_num_devices (ACC_DEVICE_NVIDIA)
   if (ngpus .eq. 0) then
     print *,'No GPUs found on this system.  Exiting'
+    call mpi_barrier (MPI_COMM_WORLD, ierr)
     call mpi_abort (MPI_COMM_WORLD,1,ierr)
   endif
   write(6,*)'myrank=',myrank,' ngpus=',ngpus
+  call mpi_barrier (MPI_COMM_WORLD, ierr)
   devicenum = mod(myrank,ngpus)
   write(6,*)'JR calling acc_set_device_num(',devicenum,')'
+  call mpi_barrier (MPI_COMM_WORLD, ierr)
   call acc_set_device_num (devicenum,ACC_DEVICE_NVIDIA)
   print *,'ngpus = ',ngpus,' rank = ',myrank,' device = ',acc_get_device_num(ACC_DEVICE_NVIDIA)
+  call mpi_barrier (MPI_COMM_WORLD, ierr)
 
   if (myrank == 0) then
     call getval (maxthreads_gpu, 'maxthreads_gpu')
