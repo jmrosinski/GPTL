75c66e959d3d8964ff19977ff683d13c2eddef70
Ed Hartnett <edwardjameshartnett@gmail.com>
2018-05-23 08:05:17 -0600

    fixed running of pmpi test

--- ctests/run_par_pmpi_test.sh
+++ ctests/run_par_pmpi_test.sh
@@ -2,11 +2,11 @@
 # This is a test script for the GPTL package. It tests the parallel
 # functionality with the MPI library.
 #
-# Ed Hartnett 5/21/18
+# Ed Hartnett 5/23/18
 
 set -e
 echo
-echo "Testing MPI summary..."
-mpiexec -n 4 ./summary
+echo "Testing MPI functionality..."
+mpiexec -n 4 ./pmpi
 echo "SUCCESS!"
 exit 0
